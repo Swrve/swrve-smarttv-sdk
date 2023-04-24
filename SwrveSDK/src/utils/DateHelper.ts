c134ce533810593fe347932bb24db43bbe65172f
Kevin Brodigan <kevin.brodigan@swrve.com>
2023-04-24 12:43:11 +0100

    Release release-1_1_7

--- SwrveSDK/src/utils/DateHelper.ts
+++ SwrveSDK/src/utils/DateHelper.ts
@@ -8,7 +8,8 @@ abstract class DateHelper {
     }
   
     public static dateToUTCDate(date: Date): Date {
-      return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+      return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), 
+      date.getUTCSeconds(), date.getUTCMilliseconds() ));
     }
 
     public static nowInUtcTime(): number {
